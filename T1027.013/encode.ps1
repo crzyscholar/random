3812c94765da7146ffaafa82557a206bc6fa4453
koka <kokabitsadze@gmail.com>
2024-11-21 21:37:55 +0400

    moved stuff around

--- T1027.013/encode.ps1
+++
@@ -1,8 +0,0 @@
-$inputFile = "C:\path\to\harmless_script.ps1"
-$outputFile = "C:\path\to\harmless_script.b64"
-
-$plainText = Get-Content -Path $inputFile -Raw
-$bytes = [System.Text.Encoding]::UTF8.GetBytes($plainText)
-$encodedText = [System.Convert]::ToBase64String($bytes)
-Set-Content -Path $outputFile -Value $encodedText
-
